d8dee1a2d1a6ee161c094d2c1c51c1ee4417051c
nix <nhoxqd@gmail.com>
2024-01-26 21:30:25 +0700

    Hello

--- DemoBai1/DemoBai1/Program.cs
+++ DemoBai1/DemoBai1/Program.cs
@@ -4,14 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int x = 4;
-
-            Console.Write("Nhap mot so nguyen: ");
-            int y = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Phuong trinh");
-            int A = x * x + y * y;
-            Console.WriteLine(A);
+            Console.Write("Hello World!");
         }
     }
 }
