799df69363c29f80f2985e3689f2a48f8802873a
phankiet <phanduykiet1@gmail.com>
2024-01-26 21:23:35 +0700

    chua_fix

--- DemoBai1/DemoBai1/obj/Debug/net6.0/DemoBai1.AssemblyInfo.cs
+++ DemoBai1/DemoBai1/obj/Debug/net6.0/DemoBai1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoBai1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bc5536f58c9c724e80f15a113315aaba533a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf985b35645d60caf1e3a1f757e2bd42cecceda")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoBai1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoBai1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
